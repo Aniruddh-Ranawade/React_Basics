24ab8353dfc5beb0865eef57445dab2778a00376
Aniruddh Ranawade <ranawadeaniruddh@gmail.com>
2023-10-23 19:26:02 +0530

    create increase/decrease counter using useState() hook

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,33 @@
+
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+
+  const [count, setCount] = useState(0);
+
+  const addValue = () => {
+    setCount(count+1);
+    console.log('clicked : ', count);
+  }
+
+  const decreaseValue = () => {
+    setCount(count-1);
+  }
+  
+  return (
+    <>
+      <h1>Counter Project</h1>
+      <br></br>
+      <br></br>
+
+      <h2>Counter Value : {count} </h2>
+      <button onClick={addValue}>Increase</button>
+      <br />
+      <br />
+      <button onClick={decreaseValue}>Decrease</button>
+    </>
+  )
+}
+
+export default App
